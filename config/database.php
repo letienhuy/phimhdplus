c34dbcead8810e1b2711014c49fb9151d969df2b
Huy Le Tien <huyletien@outlook.com>
2018-05-16 23:33:09 +0700

    Update

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_ee78ad5e320410d'),
+            'username' => env('DB_USERNAME', 'ba6699c33fbc92'),
+            'password' => env('DB_PASSWORD', 'cc5c53ad'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
